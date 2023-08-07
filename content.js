1f7dde9f70ad1b37531dc7a33806f1cb15a1cf11
DeadHello <79786672+s1072489@users.noreply.github.com>
2023-08-07 13:13:53 +1000

    Update initial files + Add index.html

--- content.js
+++ content.js
@@ -1,8 +1,1 @@
-let videos = document.querySelectorAll('video');
-videos.forEach((video) => {
-    video.style.display = 'none';
-});
-
-
-// let videoSrc = Array.from(videos).map(video => video.src);
-// browser.runtime.sendMessage({videoSrc: videoSrc});
+console.log("is this working")
